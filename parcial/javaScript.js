df998fad303431c8a9e1568dcf36361d462f1518
renejal <beimarjal@unicauca.edu.co>
2019-06-01 15:05:29 -0500

    cuando carga la interfaz piede el codigo y se actualizo es javascript

--- parcial/javaScript.js
+++ parcial/javaScript.js
@@ -1,6 +1,50 @@
 
 function buscar_codigo(){
-    edt=document.getElementById("codigo_a_buscar");
+    var edt;
+    if(document.getElementById("codigo_a_buscar")!= null){
+       
+        edt=document.getElementById("codigo_a_buscar");
+        var codigo = edt.value;
+        $.ajax({
+                url:'server.php',
+                method:'POST',
+                data:{'codigo':codigo}
+            
+        }).done(function(respuesta) {
+            /*Mostras la respuesta DOM*/ 
+            informacion_es=document.getElementById("informacion");
+            informacion_es.innerHTML=respuesta;
+        }).fail(function(jqXHR) {
+            alert(jqXHR.statusText);
+        });
+
+    }
+    if(document.getElementById("codigo_a_buscarmodal")!= null){
+
+        edt=document.getElementById("codigo_a_buscarmodal");
+
+        var codigo = edt.value;
+        $.ajax({
+                url:'server.php',
+                method:'POST',
+                data:{'codigo':codigo}
+            
+        }).done(function(respuesta) {
+            /*Mostras la respuesta DOM*/ 
+            informacion_es=document.getElementById("informacion");
+            informacion_es.innerHTML=respuesta;
+        }).fail(function(jqXHR) {
+            alert(jqXHR.statusText);
+        });
+
+    }
+
+
+
+    
+}
+function buscar_codigomodal(){
+    edt=document.getElementById("codigo_a_buscarmodal");
     var codigo = edt.value;
     $.ajax({
             url:'server.php',
@@ -34,3 +78,6 @@ function cambiar_estado_materia(comp){
         alert(jqXHR.statusText);
     });
 }
+$(document).ready(function(){
+    $('#myModal').modal('show');
+});
